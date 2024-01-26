dfddcca497ea094383dac4f39f975c55461d0c2b
Armando Pagliara <armpag@gmail.com>
2024-01-26 15:46:05 +0100

    nuovi file di sviluppo
    new class docente, with two types of JSON returned

---
+++ test/class_docente.php
@@ -0,0 +1,57 @@
+<?php
+// Definizione di una classe Docente per la gestione degli attributi
+class Docente {
+    private $codice;
+    private $nome;
+    private $cognome;
+
+    // Costruttore
+    public function __construct($codice, $nome, $cognome) {
+        $this->codice = $codice;
+        $this->nome = $nome;
+        $this->cognome = $cognome;
+    }
+
+    // Metodi get
+    public function getCodice() {
+        return $this->codice;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function getCognome() {
+        return $this->cognome;
+    }
+
+    // Metodi set
+    public function setCodice($codice) {
+        $this->codice = $codice;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function setCognome($cognome) {
+        $this->cognome = $cognome;
+    }
+	
+	// Metodo per la serializzazione in JSON
+    public function toJSON() {
+		return json_encode($this->toArray());
+    }
+
+    public function toArray() {
+		$temp = array(
+			'codice_docente' => $this->codice,
+			'nome' => $this->nome,
+			'cognome' => $this->cognome
+		);
+		return $temp;
+    }
+
+}
+
+?>
